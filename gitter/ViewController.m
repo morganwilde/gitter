a8c336c2e7100f6f4a984b2a618c77a778c164ca
Morgan Wilde <wilde.morgan@gmail.com>
2014-05-22 16:23:14 +0300

    mod 1

--- gitter/ViewController.m
+++ gitter/ViewController.m
@@ -9,6 +9,7 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UILabel *label;
 
 @end
 
@@ -17,7 +18,7 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+	self.label.text = @"This is new!";
 }
 
 - (void)didReceiveMemoryWarning
