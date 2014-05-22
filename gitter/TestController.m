1469ea0dc57595328aa9a7bfc4da0f8b84b2b8a6
Morgan Wilde <wilde.morgan@gmail.com>
2014-05-22 16:26:32 +0300

    added view 2

---
+++ gitter/TestController.m
@@ -0,0 +1,50 @@
+//
+//  TestController.m
+//  gitter
+//
+//  Created by Morgan Wilde on 22/05/2014.
+//  Copyright (c) 2014 morganwilde. All rights reserved.
+//
+
+#import "TestController.h"
+
+@interface TestController ()
+@property (weak, nonatomic) IBOutlet UILabel *label;
+
+@end
+
+@implementation TestController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    NSLog(@"changes");
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
