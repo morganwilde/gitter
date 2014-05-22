be20005cd63ee9556d3fb1e01b684a32f32dce6d
Morgan Wilde <wilde.morgan@gmail.com>
2014-05-22 16:19:48 +0300

    Initial Commit

---
+++ gitter/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  gitter
+//
+//  Created by Morgan Wilde on 22/05/2014.
+//  Copyright (c) 2014 morganwilde. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
